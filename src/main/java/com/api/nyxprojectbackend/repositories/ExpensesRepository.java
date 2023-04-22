f44f345962bda125cc028ba6b114218622781ad1
Murilo Gois <murilogois59@gmail.com>
2023-04-22 14:27:22 -0300

    feat: Create Expense Repository file
    Signed-off-by: Murilo Gois <murilogois59@gmail.com>

---
+++ src/main/java/com/api/nyxprojectbackend/repositories/ExpensesRepository.java
@@ -0,0 +1,15 @@
+package com.api.nyxprojectbackend.repositories;
+
+import com.api.nyxprojectbackend.models.ExpensesModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ExpensesRepository extends JpaRepository<ExpensesModel, UUID> {
+
+    Page<ExpensesModel> findByMesMovimentacao(Pageable pageable, int mesMovimentacao);
+
+    Page<ExpensesModel> findByCategoriaEconomicaCodigo(Pageable pageable, int categoriaEconomicaCodigo);
+}
