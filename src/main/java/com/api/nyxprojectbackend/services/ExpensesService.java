58438f663dd52eee5ae1e865034d94438fa05150
Murilo Gois de Almeida (tools) <39069727+goismurilo@users.noreply.github.com>
2023-04-22 14:27:09 -0300

    feat: Create Expense Service file
    Signed-off-by: Murilo Gois <murilogois59@gmail.com>

---
+++ src/main/java/com/api/nyxprojectbackend/services/ExpensesService.java
@@ -0,0 +1,28 @@
+package com.api.nyxprojectbackend.services;
+
+import com.api.nyxprojectbackend.models.ExpensesModel;
+import com.api.nyxprojectbackend.repositories.ExpensesRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExpensesService {
+    final ExpensesRepository expensesRepository;
+
+    public ExpensesService(ExpensesRepository expensesRepository) {
+        this.expensesRepository = expensesRepository;
+    }
+
+    public Page<ExpensesModel> findAll(Pageable pageable) {
+        return expensesRepository.findAll(pageable);
+    }
+
+    public Page<ExpensesModel> findByMonth(Pageable pageable, int mesMovimentacao) {
+        return expensesRepository.findByMesMovimentacao(pageable, mesMovimentacao);
+    }
+
+    public Page<ExpensesModel> findByCategory(Pageable pageable, int categoriaEconomicaCodigo) {
+        return expensesRepository.findByCategoriaEconomicaCodigo(pageable, categoriaEconomicaCodigo);
+    }
+}
