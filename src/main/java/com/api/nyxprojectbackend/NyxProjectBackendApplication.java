fcb6bb72bf505ae4201f04a3f6ce29f5d43cab62
Murilo Gois <murilogois59@gmail.com>
2023-04-22 00:55:12 -0300

    build: Spring project initial structure

---
+++ src/main/java/com/api/nyxprojectbackend/NyxProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.api.nyxprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NyxProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NyxProjectBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/nyxprojectbackend/NyxProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.nyxprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NyxProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
