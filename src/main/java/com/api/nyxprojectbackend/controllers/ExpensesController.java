e8843de8cba0e70743fb6c7b701a3014dfe1e7da
Murilo Gois <murilogois59@gmail.com>
2023-04-22 14:29:31 -0300

    feat: Create Expense Controller file
    Signed-off-by: Murilo Gois <murilogois59@gmail.com>

---
+++ src/main/java/com/api/nyxprojectbackend/controllers/ExpensesController.java
@@ -0,0 +1,41 @@
+package com.api.nyxprojectbackend.controllers;
+
+import com.api.nyxprojectbackend.models.ExpensesModel;
+import com.api.nyxprojectbackend.services.ExpensesService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/expenses")
+public class ExpensesController {
+    final ExpensesService expensesService;
+
+    public ExpensesController(ExpensesService expensesService) {
+        this.expensesService = expensesService;
+    }
+
+    @GetMapping("")
+    public ResponseEntity<Page<ExpensesModel>> getAllExpenses(@PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.ASC)Pageable pageable) {
+        return ResponseEntity.status(HttpStatus.OK).body(expensesService.findAll(pageable));
+    }
+
+    @GetMapping("/month/{month}")
+    public ResponseEntity<Page<ExpensesModel>> getExpensesByMonth(@PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.ASC) Pageable pageable,
+                                                                  @PathVariable(value = "month") int mesMovimentacao) {
+        return ResponseEntity.status(HttpStatus.OK).body(expensesService.findByMonth(pageable, mesMovimentacao));
+    }
+
+    @GetMapping("/category/{category}")
+    public ResponseEntity<Page<ExpensesModel>> getExpensesByCategory(@PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.ASC) Pageable pageable,
+                                                                     @PathVariable(value = "category") int categoriaEconomicaCodigo) {
+        return ResponseEntity.status(HttpStatus.OK).body(expensesService.findByCategory(pageable, categoriaEconomicaCodigo));
+    }
+}
