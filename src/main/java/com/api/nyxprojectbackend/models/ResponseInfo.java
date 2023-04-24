272e4b8cae9da29344797f77741fd7c2699a3fe1
Murilo Gois <murilogois59@gmail.com>
2023-04-24 20:43:00 -0300

    feat: create Model for responses
    Signed-off-by: Murilo Gois <murilogois59@gmail.com>

---
+++ src/main/java/com/api/nyxprojectbackend/models/ResponseInfo.java
@@ -0,0 +1,70 @@
+package com.api.nyxprojectbackend.models;
+
+import java.math.BigDecimal;
+
+public class ResponseInfo {
+    private Long numbers;
+    private int code;
+    private String name;
+
+    private BigDecimal pledged;
+    private BigDecimal paidOff;
+    private BigDecimal paid;
+
+    public ResponseInfo(Long numbers, Integer code, String name, BigDecimal pledged, BigDecimal paidOff, BigDecimal paid) {
+        setNumbers(numbers);
+        setCode(code);
+        setName(name);
+        setPledged(pledged);
+        setPaidOff(paidOff);
+        setPaid(paid);
+    }
+
+    public Long getNumbers() {
+        return numbers;
+    }
+
+    public void setNumbers(Long numbers) {
+        this.numbers = numbers;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPledged() {
+        return pledged;
+    }
+
+    public void setPledged(BigDecimal pledged) {
+        this.pledged = pledged;
+    }
+
+    public BigDecimal getPaidOff() {
+        return paidOff;
+    }
+
+    public void setPaidOff(BigDecimal paidOff) {
+        this.paidOff = paidOff;
+    }
+
+    public BigDecimal getPaid() {
+        return paid;
+    }
+
+    public void setPaid(BigDecimal paid) {
+        this.paid = paid;
+    }
+}
