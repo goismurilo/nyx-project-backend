7d7698ae780445e65c5e2e774a3428b91d38e746
Murilo Gois <murilogois59@gmail.com>
2023-04-25 04:41:25 -0300

    feat: add model for formatting responses
    Signed-off-by: Murilo Gois <murilogois59@gmail.com>

---
+++ src/main/java/com/api/nyxprojectbackend/models/RequestInfoModel.java
@@ -0,0 +1,13 @@
+package com.api.nyxprojectbackend.models;
+
+public class RequestInfoModel {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
